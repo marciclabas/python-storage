7d4d02e019208851d904797b38547312cc807889
m4rs <marcel@marcelclaramunt.cat>
2024-05-21 17:19:53 +0200

    fixed SimpleKV with new inheritance setup

--- kv/kv-api/src/kv/api/impl.py
+++ kv/kv-api/src/kv/api/impl.py
@@ -1,4 +1,4 @@
-from typing_extensions import Generic, TypeVar, AsyncIterable, Never
+from typing_extensions import Awaitable, Generic, Sequence, TypeVar, AsyncIterable, Never
 from haskellian import Left, Right, Either, either as E, promise as P, asyn_iter as AI
 from .kv import KV
 from .errors import InexistentItem, DBError, InvalidData, ReadError
@@ -11,28 +11,29 @@ class SimpleKV(KV[T], Generic[T]):
   def __init__(self, items: dict[str, T] = {}):
     self.xs = items
 
-  @P.lift
-  async def insert(self, key: str, value: T) -> Right[Never, None]:
+  async def _insert(self, key: str, value: T) -> Right[Never, None]:
     self.xs[key] = value
     return Right(None)
 
-  @P.lift
-  async def has(self, key: str) -> Either[DBError, bool]:
+  async def _has(self, key: str) -> Either[DBError, bool]:
     return Right(key in self.xs)
 
-  @P.lift
-  async def read(self, key: str) -> Either[ReadError, T]:
+  async def _read(self, key: str) -> Either[ReadError, T]:
     return E.maybe(self.xs.get(key)).mapl(lambda _: InexistentItem(key))
 
-  @P.lift
-  async def delete(self, key: str) -> Either[DBError | InexistentItem, None]:
+  async def _delete(self, key: str) -> Either[DBError | InexistentItem, None]:
     if not key in self.xs:
       return Left(InexistentItem(key))
     else:
       del self.xs[key]
       return Right(None)
+    
+  async def _keys(self) -> Either[DBError, Sequence[str]]:
+    return Right(list(self.xs.keys()))
 
-  @AI.lift
-  async def items(self, batch_size: int | None = None) -> AsyncIterable[Either[DBError | InvalidData, tuple[str, T]]]:
+  async def _items(self, batch_size: int | None = None) -> AsyncIterable[Either[DBError | InvalidData, tuple[str, T]]]:
     for item in self.xs.items():
       yield Right(item)
+
+
+SimpleKV()
